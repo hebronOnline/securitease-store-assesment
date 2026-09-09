578f4ea6f76685e4e7fcdeee2e075b930885260b
Hebron Ncabane <muzi.hebron@gmail.com>
2026-09-09 22:21:02 +0200

    Added entity graph attribute paths to optimise queries

--- src/main/java/com/example/store/repository/CustomerRepository.java
+++ src/main/java/com/example/store/repository/CustomerRepository.java
@@ -2,11 +2,17 @@ package com.example.store.repository;
 
 import com.example.store.entity.Customer;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
+    @Override
+    @EntityGraph(attributePaths = "orders")
+    List<Customer> findAll();
+
+    @EntityGraph(attributePaths = "orders")
     List<Customer> findByNameContainingIgnoreCase(String name);
 }
